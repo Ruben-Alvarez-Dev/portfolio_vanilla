d5e7e6a1288b842cd27fd4cc962eee92524ccc4e
ruben <ruben@IMACPRO.local>
2024-02-04 12:22:06 +0100

    card component working

---
+++ components/main/card/card.js
@@ -0,0 +1,12 @@
+import "./card.css";
+export const card = () => {
+  return `
+    <article class="card">
+        <h3 class="card__title">Card Title</h3>
+        <div class="card__content">
+            <p>Lorem ipsum qwfqwjf qwfb laifub qwpogub wpofgw bfonbLorem ipsum wqeffqwf qwfqwjf qwfb laifub wapoiuf bqwipofb qwpogub wpofgw bfonb</p>
+            <img class="card__content--img" src="https://via.placeholder.com/150" alt="Imagen de prueba">  
+        </div>
+    </article>
+    `;
+};
